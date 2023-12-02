1c5d7f3eb711c719f60eaab3568ad5296035bfb6
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-12-02 16:35:18 +0100

    responsive

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,16 +6,47 @@ import {Textarea} from "@nextui-org/react";
 
 function HomePage() {
   const [step, setStep] = React.useState(1);
+  const [globalContext, setGlobalContext] = React.useState(""); // State variable for user input
   const [userSituation, setUserSituation] = React.useState(""); // State variable for user input
 
   const handleSituationChange = (e) => {
     setUserSituation(e.target.value); 
     console.log(userSituation);
   };
+  const onSubmit = () => {
+    setStep(2);
+  }
   return (
-      <div className="w-full h-full bg-gradient-to-b from-[#000435] to-[#28002E] items-center justify-center">
+    <>
+    <Head>
+      <title>What Insurance could I need?</title>
+    </Head>
+    <div className="flex w-full h-screen items-center justify-center bg-gradient-to-b from-[#000435] to-[#28002E]">
         {step === 1 && (
-          <div className="self-center flex flex-col sm:p-20 p-5 sm:m-0 m-2 sm:w-[700px] w-[490px] sm:h-[700px]  h-[600px] sm:min-w-[200px] bg-[#000000] bg-opacity-50 rounded-[40px] items-center justify-center">
+          <div className="self-center flex flex-col sm:p-20 p-5 sm:m-0 m-2 sm:w-[700px] w-[490px] sm:h-[650px] h-[600px] sm:min-w-[200px] bg-[#000000] bg-opacity-50 rounded-[40px] items-center justify-center">
+            <h1 className="text-center font-bold text-[4vh] mb-10">
+              What Insurance could you need?
+            </h1>
+            <Textarea
+              label="Tell us"
+              variant="bordered"
+              placeholder="Enter your situation"
+              disableAnimation
+              disableAutosize
+              value={userSituation} // Bind state to Textarea
+              onChange={handleSituationChange} // Set the change handler
+              classNames={{
+                base: "sm:w-[600px] w-[400px]",
+                input: "resize-y min-h-[190px]",
+              }}
+            />
+            <Button className="bg-[#9932CC] mt-10" onPress={onSubmit}>
+              Submit
+            </Button>
+          </div>    
+        )} 
+        {step === 2 && (
+          <div className="self-center flex flex-col sm:p-20 p-5 sm:m-0 m-2 sm:w-[700px] w-[490px] sm:h-[650px] h-[600px] sm:min-w-[200px] bg-[#000000] bg-opacity-50 rounded-[40px] items-center justify-center">
             <h1 className="text-center font-bold text-[4vh] mb-10">
               Describe your situation
             </h1>
@@ -32,14 +63,14 @@ function HomePage() {
                 input: "resize-y min-h-[190px]",
               }}
             />
-            <Button className="bg-[#9932CC] mt-10">
+            <Button className="bg-[#9932CC] mt-10" onPress={onSubmit}>
               Submit
             </Button>
           </div>    
         )} 
 
       </div>
-    
+    </>
   );
 }
 
