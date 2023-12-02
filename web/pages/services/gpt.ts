bedded463fb8d0729d583df8f102d8df15c1803a
Gerard <gerardmonso@gmail.com>
2023-12-02 18:34:31 +0100

    new gpt class

--- web/pages/services/gpt.ts
+++ web/pages/services/gpt.ts
@@ -5,6 +5,8 @@ export class GPTChatService {
   private messages: any;
 
   constructor() {
+    //this.apiUrl = process.env.CHATGPT_PUBLIC_API_URL;
+    //this.apiKey = process.env.CHATGPT_PUBLIC_API_KEY;
     this.apiUrl = "https://api.openai.com";
     this.apiKey = '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';
     this.messages = [];
@@ -14,16 +16,17 @@ export class GPTChatService {
     this.messages.push({ role: 'user', content: 'create a story from this prompt: \n' + prompt }); // donam dues paraules claus per continuar la historia
 
     debugger;
-    var responseBody = await this.sendMessages(this.messages);
+    var responseBody = await this.sendMessages();
     var message = responseBody.choices[0].message.content;
     return message;
   }
 
   public async getGptStorieOptions(): Promise<string[]> {
+    debugger;
     this.messages.push({ role: 'system', content: 'Give me two words separated by a comma that are key to continuing the story, then they will be used to further generate the story.' }); // donam dues paraules claus per continuar la historia
     
     debugger;
-    var responseBody = await this.sendMessages(this.messages);
+    var responseBody = await this.sendMessages();
     var keys = responseBody.choices[0].message.content;
     if (keys.includes(',')) {
       keys = keys.split(',');
@@ -35,7 +38,7 @@ export class GPTChatService {
   }
 
 
-  public async sendMessages(messages: { role: string; content: string }[]): Promise<string[]> {
+  public async sendMessages(): Promise<any> {
     const url = `${this.apiUrl}/v1/chat/completions`;
     const headers = {
       'Content-Type': 'application/json',
@@ -45,14 +48,14 @@ export class GPTChatService {
 
     const body = JSON.stringify({
       model: 'gpt-3.5-turbo',
-      messages: messages,
-      //temperature: 1,
-      //top_p: 1,
-      //n: 1,
-      //stream: false,
-      //max_tokens: 250,
-      //presence_penalty: 0,
-      //frequency_penalty: 0,
+      messages: this.messages,
+      temperature: 1,
+      top_p: 1,
+      n: 1,
+      stream: false,
+      max_tokens: 500,
+      presence_penalty: 0,
+      frequency_penalty: 0,
     });
 
     try {
@@ -62,16 +65,18 @@ export class GPTChatService {
         body: body,
       });
 
+      debugger;
       if (!response.ok) {
         const errorText = await response.text();
-        throw new Error(`Failed to send messages: ${errorText}`);
+
+        //throw new Error(`Failed to send messages: ${errorText}`);
 
       }
       const responseBody = await response.json();
       return responseBody;
     } catch (error) {
       console.error('Error sending messages:', error);
-      throw error;
+      //throw error;
     }
   }
 
