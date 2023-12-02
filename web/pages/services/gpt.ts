13ca515acae442cbaf5e27a077c385e76f49a3b0
Gerard <gerardmonso@gmail.com>
2023-12-02 17:52:12 +0100

    new gpt class

--- web/pages/services/gpt.ts
+++ web/pages/services/gpt.ts
@@ -2,20 +2,40 @@
 export class GPTChatService {
   private apiUrl: string;
   private apiKey: string;
+  private messages: any;
 
   constructor() {
     this.apiUrl = "https://api.openai.com";
     this.apiKey = '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';
+    this.messages = [];
+  }
+
+  public async initGptStorie(prompt: string): Promise<string> {
+    this.messages.push({ role: 'user', content: 'create a story from this prompt: \n' + prompt }); // donam dues paraules claus per continuar la historia
+
+    debugger;
+    var responseBody = await this.sendMessages(this.messages);
+    var message = responseBody.choices[0].message.content;
+    return message;
+  }
+
+  public async getGptStorieOptions(): Promise<string[]> {
+    this.messages.push({ role: 'system', content: 'Give me two words separated by a comma that are key to continuing the story, then they will be used to further generate the story.' }); // donam dues paraules claus per continuar la historia
+    
+    debugger;
+    var responseBody = await this.sendMessages(this.messages);
+    var keys = responseBody.choices[0].message.content;
+    if (keys.includes(',')) {
+      keys = keys.split(',');
+    } else {
+      return ['creative', 'pessimist'];
+    }
+    
+    return keys;
   }
 
 
-  public async getGptStorieOptions(messages: { role: string; content: string }[]): Promise<any> {
-    messages.push({ role: 'system', content: 'Give me two words separated by a comma that are key to continuing the story, then they will be used to further generate the story.' }); // donam dues paraules claus per continuar la historia
-    return this.sendMessages(messages);
-  }
-
-
-  public async sendMessages(messages: { role: string; content: string }[]): Promise<[]> {
+  public async sendMessages(messages: { role: string; content: string }[]): Promise<string[]> {
     const url = `${this.apiUrl}/v1/chat/completions`;
     const headers = {
       'Content-Type': 'application/json',
@@ -42,27 +62,16 @@ export class GPTChatService {
         body: body,
       });
 
-      debugger;
       if (!response.ok) {
-        //const errorText = await response.text();
-        //throw new Error(`Failed to send messages: ${errorText}`);
-        return ['creative', 'pessimist'];
+        const errorText = await response.text();
+        throw new Error(`Failed to send messages: ${errorText}`);
 
       }
-
       const responseBody = await response.json();
-      var keys = responseBody.choices[0].message.content;
-
-      if (keys.includes(',')) {
-        keys = keys.split(',');
-      } else {
-        return ['creative', 'pessimist'];
-      }
-      return keys;
+      return responseBody;
     } catch (error) {
-      //console.error('Error sending messages:', error);
-      //throw error;
-      return ['creative', 'pessimist'];
+      console.error('Error sending messages:', error);
+      throw error;
     }
   }
 
