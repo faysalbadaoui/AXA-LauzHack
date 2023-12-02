9fc82e75769b6758ee1f3c944519704d37a82c8c
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-12-02 18:37:40 +0100

    added purple as primary

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Head from "next/head";
 import { Button } from "@nextui-org/button";
-import {Textarea} from "@nextui-org/react";
+import {Spinner, Textarea} from "@nextui-org/react";
 import MessageArea from "../components/textArea.js";
 import {GPTChatService} from "./services/gpt.ts";
 
@@ -10,21 +10,32 @@ function HomePage() {
   const [step, setStep] = React.useState(1);
   const [userSituation, setUserSituation] = React.useState(""); // State variable for user input
   const gptService = new GPTChatService(); // State variable for user input
-  const handleSituationChange = (e) => {
+  const [clicked, setClicked] = React.useState(false); // State variable for user input
+  const handleSituationChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setUserSituation(e.target.value); 
     console.log(userSituation);
   };
+
   const onSubmit = () => {
-    const result = gptService.getGptStorieOptions([{ role: 'user', content: 'Va un gos i cau.' }, { role: 'assistant', content: 'Un gos verd anava al cole' },{ role: 'system', content: 'Give me two words separated by a comma that are key to continuing the story, then they will be used to further generate the story.' }]);
+    setClicked(true);
+    const result = gptService.initGptStorie(userSituation)
+      .then(result => {
+        console.log(result);
+        setClicked(false);
+        setStep(2);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setClicked(false);
+      });  
     console.log(result);
-    setStep(2);
   }
   return (
     <>
     <Head>
       <title>What Insurance could I need?</title>
     </Head>
-    <div className="flex w-full h-screen items-center justify-center bg-gradient-to-b from-[#000435] to-[#28002E]">
+    <div className="flex w-full flex-col h-screen items-center justify-center bg-gradient-to-b from-[#000435] to-[#28002E]">
         {step === 1 && (
           <div className="self-center flex flex-col sm:p-20 p-5 sm:m-0 m-2 sm:w-[700px] w-[490px] sm:h-[650px] h-[600px] sm:min-w-[200px] bg-[#000000] bg-opacity-50 rounded-[40px] items-center justify-center">
             <h1 className="text-center font-bold text-[4vh] mb-10">
@@ -43,9 +54,14 @@ function HomePage() {
                 input: "resize-y min-h-[190px]",
               }}
             />
-            <Button className="bg-[#9932CC] mt-10" onPress={onSubmit}>
-              Submit
-            </Button>
+            {clicked && (
+              <Spinner className="mt-10" color="primary" />
+            )}
+            {!clicked && (
+              <Button className="bg-[#9932CC] mt-10" onPress={onSubmit}>
+                Submit
+              </Button>
+            )}
           </div>    
         )} 
         {step === 2 && (
@@ -53,13 +69,11 @@ function HomePage() {
             <h1 className="text-center font-bold text-[4vh] mb-10">
               Here's what Insunator thinks:
             </h1>
-            <MessageArea text="You should get a car insurance sad
-            d asd asdsa sadsadddddddddddddar insurance sad
-            " />
+            <MessageArea text=""/>
           </div>    
-          
         )} 
 
+        
       </div>
     </>
   );
