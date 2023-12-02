6b874a7fc73ae9d27d96164f0660dc37a4a35c41
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-12-02 16:10:28 +0100

    first step

--- web/layouts/layout.js
+++ web/layouts/layout.js
@@ -4,10 +4,8 @@ import Head from 'next/head';
 export default function RootLayout({ children }) {
   return (
     
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <div style={{ flex: 1, overflowY: "auto" }}>
+    <div style={{ display: "flex", flexDirection: "column"}} className="items-center justify-center w-full h-full">
         {children}
-        </div>
     </div>
   );
 }
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,10 +6,10 @@ function HomePage() {
   const [step, setStep] = React.useState(1);
 
   return (
-      <div className="w-full h-full bg-gradient-to-b from-[#000435] to-[#28002E]">
+      <div className="w-full h-full bg-gradient-to-b from-[#000435] to-[#28002E] items-center justify-center">
         {step === 1 && (
-          <div className="flex flex-col items-center justify-center h-[80vh] p-20 w-[90vw] min-w-[800px] max-w-[1200px] bg-[#000000] bg-opacity-50">
-            <h1>
+          <div className="self-center flex flex-col h-[80vh] p-20 w-[90vw] min-w-[800px] max-w-[1200px] bg-[#000000] bg-opacity-50 rounded-[40px]">
+            <h1 className="text-center font-bold text-[4vh]">
               Describe your situation
             </h1>
           </div>    
