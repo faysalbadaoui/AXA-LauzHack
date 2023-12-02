81957167fa4a96661f6b2b36dd2fb94b54f7f6cc
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-12-02 15:53:40 +0100

    starting the process

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,9 +3,18 @@ import React from "react";
 import Head from "next/head";
 import { Button } from "@nextui-org/button";
 function HomePage() {
+  const [step, setStep] = React.useState(1);
+
   return (
-      <div className="bg-gradient-to-r from-blue-900 to-blue-600">
-        <Button> Click </Button>
+      <div className="w-full h-full bg-gradient-to-b from-[#000435] to-[#28002E]">
+        {step === 1 && (
+          <div className="flex flex-col items-center justify-center h-[80vh] p-20 w-[90vw] min-w-[800px] max-w-[1200px] bg-[#000000] bg-opacity-50">
+            <h1>
+              Describe your situation
+            </h1>
+          </div>    
+        )} 
+
       </div>
     
   );
