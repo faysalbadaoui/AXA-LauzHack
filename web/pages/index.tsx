108fd64a5a09abeace100f31c588730cfa153243
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-12-03 00:34:01 +0100

    start smart contract impl

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,28 +1,47 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { Button } from "@nextui-org/button";
 import {Spinner, Textarea} from "@nextui-org/react";
 import MessageArea from "../components/textArea.js";
 import {GPTChatService} from "../components/gpt";
 import Image from "next/image";
+require('dotenv').config()
 const { ethers } = require('ethers');
+const { web3} = require('web3');
+const CONTRACT_ADDRESS = "0x2492d1CeF3d23EC48ADa469003D8652375d791f0";
+
+const mintNFTUser = async () => {
+
+    if(window !== undefined){
+      try{
+          const { ethereum } = window;
+          const contractArtifact = require('../smart-contract-abi/AxaToken.json');
+          const contractABI = contractArtifact.abi;
+          const providerU = "https://thrumming-boldest-scion.ethereum-goerli.quiknode.pro/78519f495e10e3f34c65ea1d477f8dbc4de92688/";
+          const contractAddress = CONTRACT_ADDRESS;
+          const accountPrivateKey = "0f8c237210aa9b0a3a47ee8fefc4a6f5166b9b26acc6e634d24a00a255d174ca";
+          const provider = new ethers.providers.Web3Provider(providerU);
+          const signer = new ethers.Wallet(accountPrivateKey, provider);
+          const contract = new ethers.Contract(contractAddress, contractABI, signer);
+          
+          console.log("Account address: ", signer.address);
+        
+      }catch(error){
+      }
+    }
+
+}
 
 function HomePage() {
-  const contractArtifact = require('./AquaSaveToken.json');
-  const contractABI = contractArtifact.abi;
-  const providerUrl = process.env.providerUrl;
-  const contractAddress = process.env.contractAddress;
-  const accountPrivateKey = process.env.accountPrivateKey;
-  const provider = new ethers.providers.JsonRpcProvider(providerUrl);
-  const signer = new ethers.Wallet(accountPrivateKey, provider);
-  const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
+  const [isWalletConnected, setIsWalletConnected] = React.useState(false);
   const [step, setStep] = React.useState(1);
   const [userSituation, setUserSituation] = React.useState(""); 
   const gptService = new GPTChatService(); 
   const [clicked, setClicked] = React.useState(false); 
   const [story, setStory] = React.useState(""); 
+
   const handleSituationChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setUserSituation(e.target.value); 
     console.log(userSituation);
@@ -30,6 +49,20 @@ function HomePage() {
   const [buttons, setButtons] = React.useState([]);
   const [imageUrl, setImageUrl] = React.useState("");
   
+  const connectWallet = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setIsWalletConnected(true);
+        mintNFTUser();
+      } catch (error) {
+        console.error('Error connecting to wallet:', error);
+      }
+    } else {
+      console.log('Ethereum wallet is not installed');
+    }
+  };
+
   const onSubmit = () => {
     setClicked(true);
     gptService.getGptStorie(userSituation)
@@ -77,7 +110,18 @@ function HomePage() {
         <title>What Insurance could I need?</title>
       </Head>
       <div className="flex w-full flex-col h-screen items-center justify-center bg-gradient-to-b from-[#00008F] to-[#28002E]">
+          <div className="flex flex-row">
           <Image src="/res/logo.png" alt="logo" width={250} height={150} />
+          { !isWalletConnected && (
+            <Button className="bg-[#9932CC] mt-10" onPress={connectWallet}>
+              Connect Wallet
+            </Button>
+        )}
+        { isWalletConnected && (
+            <h2>Wallet Connected</h2>
+        )}
+          </div>
+          
           {step === 1 && (
             <div className="self-center flex flex-col sm:p-20 shadow shadow-red-500/90 hover:shadow-red-500/90 p-5 sm:m-0 m-2 sm:h-[650px] h-[600px] sm:min-w-[200px] bg-[#000000] bg-opacity-50 rounded-[40px] items-center justify-center">
               <h1 className="text-center font-bold sm:text-[4vh] text-[3vh] mb-10">
