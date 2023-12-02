1e6a15c1731e496c2235e53c3cef2411ad902123
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-12-02 21:48:23 +0100

    made typewriter effect

--- web/components/textArea.js
+++ web/components/textArea.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Button, Textarea, button } from "@nextui-org/react";
+import Typewriter from 'typewriter-effect'; 
+
 function MessageArea({ text, buttons, onClickTheButton }) {
     const chunkSize = 10;
     const [currentText, setCurrentText] = useState('');
@@ -10,7 +12,7 @@ function MessageArea({ text, buttons, onClickTheButton }) {
     }
 
     useEffect(() => {
-      const interval = setInterval(() => {
+      /*const interval = setInterval(() => {
         if (currentIndex < text.length) {
           const nextChunk = text.slice(currentIndex, currentIndex + chunkSize);
           setCurrentText(currentText => currentText + nextChunk);
@@ -20,13 +22,23 @@ function MessageArea({ text, buttons, onClickTheButton }) {
         }
       }, 700);
   
-      return () => clearInterval(interval);
+      return () => clearInterval(interval);*/
     }, [currentIndex, text]);
   return (
     <>
-        <div className="w-full min-h-[40px] bg-white bg-opacity-10 rounded-[30px] p-4">
-        {currentText}
+        <div className="w-full min-h-[60px] bg-white bg-opacity-10 rounded-[30px] p-4">
+            <Typewriter 
+                onInit={(typewriter) => { 
+                typewriter.typeString(text) 
+                    .callFunction(() => { 
+                        setCurrentText(text);
+                    console.log('String typed out!'); 
+                    }) 
+                    .start();
+                }} 
+        /> 
         </div>
+
         {currentText == text && (
             <div className="w-full flex flex.row justify-around mt-5">
                 <Button className="bg-[#9932CC]" onClick={onClickButton(buttons[0])}>{buttons[0]}</Button>
