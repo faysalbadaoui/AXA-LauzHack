cbb8296f67eacc107647dac7abb85ff5e7f50bb3
Andreu Pérez <apt15udl@gmail.com>
2023-12-03 02:03:13 +0100

    Correct Image

--- web/components/gpt.ts
+++ web/components/gpt.ts
@@ -81,17 +81,15 @@ export class GPTChatService {
     return message;
   }
 
-  public async getGptImage(prompt: string): Promise<string> {
-    return 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg¡';
-    
+  public async getGptImage(prompt: string): Promise<string> {    
     this.prompt = 'comic book style: ' + prompt;
     var responseBody = await this.callChatGPTChatImage();
 
-    var keys : string;
     if (responseBody == false){
       return 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-HL3itsiK4BaIX5fQR2OkE8T2/user-63L3WQrKtpArmIckMx9hskYG/img-xUxEmOL6TnCLIojaMa9mcSXV.png?st=2023-12-02T21%3A14%3A44Z&se=2023-12-02T23%3A14%3A44Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-12-01T22%3A41%3A11Z&ske=2023-12-02T22%3A41%3A11Z&sks=b&skv=2021-08-06&sig=feAtpCT/BPPRWtdtESUecZ3H2/aLlTruiwhh3aWG2AE%3D';
     }
 
+    var keys = responseBody.data[0].url;
     return keys;
   }
 
@@ -153,8 +151,7 @@ export class GPTChatService {
     const body = JSON.stringify({
       model: 'dall-e-3',
       prompt: this.prompt,
-      //size: '1024x1024',
-      size: '3040x1440',
+      size: '1024x1024',
       //temperature: 1,
       //top_p: 1,
       //n: 1,
